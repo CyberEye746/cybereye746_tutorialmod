34282e5d7d40069158258c5cb2a7f616e8014dbb
CyberEye746 <cutescu.cristian@gmail.com>
2026-05-31 21:04:42 -0400

    added Nether star shards(work) added compressed nether stars a.k.a. nether star blocks (broken)

---
+++ src/main/java/net/cybereye746/tutorialmod/item/ModItems.java
@@ -0,0 +1,26 @@
+package net.cybereye746.tutorialmod.item;
+
+import net.cybereye746.tutorialmod.TutorialMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item Nether_Shard = registerItem("nether_shard", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("registering Mod Items for" + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(Nether_Shard);
+        });
+
+    }
+}
